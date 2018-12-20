97e1d059e55dbed8aa5dffccac2260290301cc98
elmira2705 <37979137+elmira2705@users.noreply.github.com>
2018-12-21 00:30:22 +0600

    Update cart.js

--- event-bubbling-capturing/shop/js/cart.js
+++ event-bubbling-capturing/shop/js/cart.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const productList = document.querySelector('.items-list');
+productList.addEventListener('click', onButtonClick, true);
+
+function onButtonClick(event) {
+  const btn = event.target;
+  if (btn.className === 'add-to-cart') {
+    addToCart({title: btn.dataset.title, price: btn.dataset.price});
+    event.stopPropagation();
+  }
+}
