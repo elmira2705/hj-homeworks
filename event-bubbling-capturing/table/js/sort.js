7740a6e027bca7e4de3bbd2865ae4ba7966f8be6
elmira2705 <37979137+elmira2705@users.noreply.github.com>
2018-12-21 00:31:23 +0600

    Update sort.js

--- event-bubbling-capturing/table/js/sort.js
+++ event-bubbling-capturing/table/js/sort.js
@@ -1,5 +1,10 @@
 'use strict';
 
 function handleTableClick(event) {
-  
+  if (event.target.tagName === 'TH') {
+    const table = document.querySelector('table');
+    table.dataset.sortBy = event.target.dataset.propName;
+    event.target.dataset.dir ? event.target.dataset.dir *= -1 : event.target.dataset.dir = 1;
+    sortTable(event.currentTarget.dataset.sortBy, event.target.dataset.dir);    
+  }
 }
