cae98e3d4b6e5b7c025da60c5f1162739c21a6ce
elmira2705 <37979137+elmira2705@users.noreply.github.com>
2019-01-02 20:54:19 +0600

    Create drum-machine.js

---
+++ html-element-collection/drum-machine/js/drum-machine.js
@@ -0,0 +1,18 @@
+function playSound() {
+  const player = this.getElementsByTagName('audio')[0];
+  player.pause();
+  player.currentTime = 0;
+  player.play();
+}
+
+function setEvents() {
+  const buttons = document.getElementsByClassName('drum-kit__drum');
+
+  if (buttons) {
+    for (const btn of buttons) {
+      btn.addEventListener('click', playSound);
+    }
+  }
+}
+
+setEvents();
