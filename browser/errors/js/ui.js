561c74626d3d697f75ba51ffd0720dbdb98873ed
elmira2705 <37979137+elmira2705@users.noreply.github.com>
2019-01-02 20:18:49 +0600

    Update ui.js

--- browser/errors/js/ui.js
+++ browser/errors/js/ui.js
@@ -1,6 +1,6 @@
 'use strict';
-function handleClick {
+function handleClick() {
   openMail();
 }
 const button = window.getElementById('open');
-button.onclick = handleClick();
+button.onclick = handleClick;
