1ef41551d6bd25ba8fc8978e692024685c87a0bb
elmira2705 <37979137+elmira2705@users.noreply.github.com>
2019-01-17 01:43:55 +0600

    Update realtime.js

--- comet/realtime/js/realtime.js
+++ comet/realtime/js/realtime.js
@@ -14,16 +14,24 @@ const realtime = new Chart(ctx).Bar({
 let isFirst = true;
 const ws = new WebSocket('wss://neto-api.herokuapp.com/realtime');
 ws.addEventListener('message', event => {
+console.log(event.data); //
+try { //
+const info = JSON.parse(event.data); //
   if (isFirst) {
-    event.data
-      .split('\n')
-      .map(line => line.split('|'))
-      .forEach(data => realtime.addData([Number(data[1])], data[0]));
+//     event.data
+//       .split('\n')
+//       .map(line => line.split('|'))
+//       .forEach(data => realtime.addData([Number(data[1])], data[0]));
 
+info.forEach(data => realtime.addData([Number(data.online)], data.time)); //
     isFirst = false;
   } else {
-    const [label, data] = event.data.split('|');
+//     const [label, data] = event.data.split('|');
     realtime.removeData();
-    realtime.addData([Number(data)], label);
+//     realtime.addData([Number(data)], label);
+realtime.addData([Number(info.online)], info.time); //
+}
+} catch (err) {//
+console.log(err);//
   }
 });
