b0f2065b8e1a3484504bef86178f3b9f5a9809a8
elmira2705 <37979137+elmira2705@users.noreply.github.com>
2019-01-23 02:43:18 +0600

    Update main.js

--- file-api/text-editor/js/main.js
+++ file-api/text-editor/js/main.js
@@ -15,30 +15,62 @@ class TextEditor {
     this.registerEvents();
     this.load( this.getStorageData());
   }
-  registerEvents() {
-    const save = throttle( this.save.bind( this ), 1000 );
-    this.contentContainer.addEventListener( 'input', save );
+   registerEvents() {
+    const save = throttle(this.save.bind(this), 1000);
+    this.contentContainer.addEventListener('input', save);
+    ['dragenter', 'dragover', 'drop'].forEach(eventName => {
+    	this.contentContainer.addEventListener(eventName, event => event.preventDefault(), false);
+    });
+    ['dragenter', 'dragover'].forEach(eventName => {
+    	this.contentContainer.addEventListener(eventName, this.showHint.bind(this), false);
+    });
+    ['dragleave', 'drop'].forEach(eventName => {
+    	this.contentContainer.addEventListener(eventName, this.hideHint.bind(this), false);
+    });
+    this.contentContainer.addEventListener('drop', this.loadFile.bind(this), false);
   }
-  loadFile( e ) {
+	
+  loadFile(event) {
+    const file = event.dataTransfer.files[0];
+    const fileType = /^text\/plain/;
+    if (file && file.type.match(fileType)) {
+			this.setFilename(file.name);
+			this.readFile(file);
+    } else {
+    	this.load('Файл не может быть прочитан.\nТолько файлы .txt');
+    }
   }
-  readFile( file ) {
+	
+  readFile(file) {
+		const reader = new FileReader();
+		this.load('');
+		reader.addEventListener('loadend', (event) => this.load(event.currentTarget.result));
+		reader.readAsText(file);
   }
-  setFilename( filename ) {
-    this.filenameContainer.textContent = filename;
+	
+  setFilename(filename) {
+  	this.filenameContainer.textContent = filename;
   }
-  showHint( e ) {
+	
+  showHint(event) {
+    this.hintContainer.classList.add('text-editor__hint_visible');
   }
-  hideHint() {
+	
+  hideHint(event) {
+		this.hintContainer.classList.remove('text-editor__hint_visible');
   }
-  load( value ) {
-    this.contentContainer.value = value || '';
+	
+  load(value) {
+  	this.contentContainer.value = value || '';
   }
+	
   getStorageData() {
-    return localStorage[ this.storageKey ];
+  	return localStorage[this.storageKey];
   }
+	
   save() {
-    localStorage[ this.storageKey ] = this.contentContainer.value;
+  	localStorage[this.storageKey] = this.contentContainer.value;
   }
 }
 
-new TextEditor( document.getElementById( 'editor' ));
+new TextEditor(document.getElementById('editor'));
